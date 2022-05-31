3170de382a2d13ada61e5102b68fc1a5703e96d5
simplilearn-github <dkakrecha07@gmail.com>
2022-05-31 17:11:21 +0530

    -this is uppercase file

---
+++ uppercase.js
@@ -0,0 +1,7 @@
+let  uc = require('upper-case');
+let http = require('http');
+http.createServer(function (req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc.upperCase("hello world"));
+    res.end();
+}).listen(8080);
